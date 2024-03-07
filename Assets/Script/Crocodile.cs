a317a3fb5493f81914d6ebb653c5df71de7d6985
Sybirk <115795460+Sybirk@users.noreply.github.com>
2024-03-07 09:57:07 +0100

    UI Start

--- Assets/Script/Crocodile.cs
+++ Assets/Script/Crocodile.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 
 public class Crocodile : MonoBehaviour
 {
-    public GameObject crocodilePos;
     private void Start()
     {
-        transform.DOMoveX(crocodilePos.transform.position.x -20f, 3f);
-    }
-    private void OnCollisionEnter(Collision other) 
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Le croco à mangé la potite grenouille");
-        }
+        gameObject.transform.DOMoveX(gameObject.transform.position.x -20f, 3f);
     }
 }
--- Assets/Script/HealthPlayer.cs
+++ Assets/Script/HealthPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HealthPlayer : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class HealthPlayer : MonoBehaviour
         if(other.CompareTag("Ennemy"))
         {
             Debug.Log("Death");
+            transform.position = new Vector3(0f,1f,0f);
         }
    }
 }

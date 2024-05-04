c6e3f5dd645ee033877b971aad4988ab0209d81d
Sybirk <115795460+Sybirk@users.noreply.github.com>
2024-05-04 19:10:34 +0200

    menu fin de niveau

--- Assets/Script/WinCondition.cs
+++ Assets/Script/WinCondition.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
+    public string SceneName;
     private void OnTriggerEnter(Collider other) 
     {
         if(other.CompareTag("Player"))
         {
             Debug.Log("Win !");
-            SceneManager.LoadScene("LevelSelectionFrogs");
+            SceneManager.LoadScene(SceneName);
         }
     }
 }

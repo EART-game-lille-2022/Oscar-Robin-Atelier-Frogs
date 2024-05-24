b1d6a46740f216de47978ae3961218d3f5fc8646
Sybirk <115795460+Sybirk@users.noreply.github.com>
2024-05-24 11:30:45 +0200

    fix level animation ARTHUR MERCI)

--- Assets/Script/Jumper.cs
+++ Assets/Script/Jumper.cs
@@ -9,11 +9,12 @@ public class Jumper : MonoBehaviour
     public float jumpHeight = 8f;
     public float jumpDuration = 1.5f;
     public float jumpForce = 8f;
+    public AnimationCurve jump;
     private void OnTriggerEnter(Collider other) 
     {
         if(other.CompareTag("Player"))
         {
-            capsule.transform.DOJump(new Vector3(capsule.transform.position.x, capsule.transform.position.y, capsule.transform.position.z+ jumpForce), jumpHeight, 1, jumpDuration);
+            capsule.transform.DOJump(new Vector3(capsule.transform.position.x, capsule.transform.position.y, capsule.transform.position.z+ jumpForce), jumpHeight, 1, jumpDuration).SetEase(jump).Play();
         }
     }
 }
